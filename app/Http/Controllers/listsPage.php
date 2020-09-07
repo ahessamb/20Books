f693977e032e1364b2d16bf65cee59b3a10dca52
Amirhessam Bolandi <amirhessamb123@yahoo.com>
2020-09-07 15:09:43 +0430

    first commit

---
+++ app/Http/Controllers/listsPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class listsPage extends Controller
+{
+    public function index(){
+        return view('listsPage');
+
+    }
+}
